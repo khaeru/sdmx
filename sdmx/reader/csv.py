d83b775408a019ebaad704e064abb1ab9ef56669
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-03-10 10:10:33 +0100

    Add tests of csv.Reader

--- sdmx/reader/csv.py
+++ sdmx/reader/csv.py
@@ -6,8 +6,8 @@ from sdmx.reader.base import BaseReader
 class Reader(BaseReader):
     """Read SDMX-CSV."""
 
-    content_types = list_media_types(base="csv")
+    media_types = list_media_types(base="csv")
     suffixes = [".csv"]
 
-    def read_message(self, source, dsd=None):
+    def read_message(self, source, dsd=None):  # pragma: no cover
         raise NotImplementedError
