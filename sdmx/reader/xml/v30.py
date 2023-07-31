6773b8ef9a92da58694d0ad407b920ebbca53d66
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-07-31 11:10:45 +0200

    Reuse SDMX-ML 2.1 parsers for 3.0.0

--- sdmx/reader/xml/v30.py
+++ sdmx/reader/xml/v30.py
@@ -1,5 +1,4 @@
 """SDMX-ML 3.0.0 reader."""
-from itertools import product
 from typing import Any, Dict
 
 import sdmx.urn
@@ -8,17 +7,6 @@ from sdmx.model import common
 from sdmx.model import v30 as model
 
 from . import v21
-
-_VERSION = Version["3.0.0"]
-
-# In SDMX-ML 3.0, individual classes of ItemScheme are collected in separate XML
-# container elements. Skip all of these.
-SKIP = """
-    str:AgencySchemes str:ConceptSchemes str:CustomTypeSchemes str:GeographicCodelists
-    str:GeoGridCodelists str:NamePersonalisationSchemes str:RulesetSchemes
-    str:TransformationSchemes str:UserDefinedOperatorSchemes str:ValueLists
-    str:VtlMappingSchemes
-"""
 
 
 class Reference(v21.Reference):
@@ -36,26 +24,50 @@ class Reference(v21.Reference):
 
 
 class Reader(v21.Reader):
-    media_types = list_media_types(base="xml", version=_VERSION)
-    xml_version = _VERSION
+    xml_version = Version["3.0.0"]
+    media_types = list_media_types(base="xml", version=xml_version)
     Reference = Reference
 
 
-v21.PARSE.update({k: None for k in product(v21.to_tags(SKIP), ["start", "end"])})
+# Rewrite the v21.Reader collection of parsers to refer to SDMX-ML 3.0.0 namespaces
+# instead of SDMX-ML 2.1
+new_parsers = dict()
+for (tag, event), func in v21.Reader.parser.items():
+    # Construct a new tag using the same prefix (e.g. "str") and local name
+    new_tag = Reader.format.qname(
+        v21.Reader.format.ns_prefix(tag.namespace), tag.localname
+    )
+    # Store a reference to the same function
+    new_parsers[(new_tag, event)] = func
+# Replace the parser collection
+Reader.parser = new_parsers
+
+# Shorthand
+start = Reader.start
+end = Reader.end
+
+# In SDMX-ML 3.0, individual classes of ItemScheme are collected in separate XML
+# container elements. Skip all of these.
+start(
+    """
+    str:AgencySchemes str:ConceptSchemes str:CustomTypeSchemes str:DataConstraints
+    str:GeographicCodelists str:GeoGridCodelists str:NamePersonalisationSchemes
+    str:RulesetSchemes str:TransformationSchemes str:UserDefinedOperatorSchemes
+    str:ValueLists str:VtlMappingSchemes
+    """
+)(None)
 
 # New qnames in SDMX-ML 3.0 parsed using existing methods from .reader.xml.v21
-v21.end("str:GeoCell str:GridDefinition")(v21._text)
-v21.end("str:GeographicCodelist str:ValueList")(v21._itemscheme)
-v21.start("str:GeoFeatureSetCode str:GeoGridCode str:ValueItem", only=False)(
+end("str:GeoCell str:GridDefinition")(v21._text)
+end("str:GeographicCodelist str:ValueList")(v21._itemscheme)
+start("str:GeoFeatureSetCode str:GeoGridCode str:ValueItem", only=False)(
     v21._item_start
 )
-v21.end("str:GeoFeatureSetCode str:GeoGridCode str:ValueItem", only=False)(
-    v21._item_end
-)
-v21.end("str:Measure")(v21._component)
+end("str:GeoFeatureSetCode str:GeoGridCode str:ValueItem", only=False)(v21._item_end)
+end("str:Measure")(v21._component)
 
 
-@v21.end("str:Codelist")
+@end("str:Codelist")
 def _cl(reader, elem):
     try:
         sdmx.urn.match(elem.text)
@@ -67,7 +79,7 @@ def _cl(reader, elem):
         reader.push(elem, elem.text)
 
 
-@v21.end("str:CodelistExtension")
+@end("str:CodelistExtension")
 def _cl_ext(reader, elem):
     return model.CodelistExtension(
         codelist=reader.pop_resolved_ref("Codelist"),
@@ -75,25 +87,25 @@ def _cl_ext(reader, elem):
     )
 
 
-@v21.end("str:ExclusiveCodeSelection str:InclusiveCodeSelection")
+@end("str:ExclusiveCodeSelection str:InclusiveCodeSelection")
 def _code_selection(reader, elem):
-    print(elem)
+    raise NotImplementedError
 
 
-@v21.end("str:MemberValue")
+@end("str:MemberValue")
 def _mv(reader, elem):
     return common.MemberValue(value=elem.text)
 
 
-@v21.end("str:GeoGridCodelist")
+@end("str:GeoGridCodelist")
 def _ggcl(reader, elem):
     result = v21._itemscheme(reader, elem)
     result.grid_definition = reader.pop_single("GridDefinition")
     return result
 
 
-@v21.end("str:GeoGridCode", only=False)
+@end("str:GeoGridCode", only=False)
 def _ggc_end(reader, elem):
-    result = v21._item(reader, elem)
+    result = v21._item_end(reader, elem)
     result.geo_cell = reader.pop_single("GeoCell")
     return result
