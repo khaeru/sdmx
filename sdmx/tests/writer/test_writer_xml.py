7f1217e298736302557ca3f6a798bc0d8cecf923
Reginald Maltais <reginald.maltais@statcan.gc.ca>
2023-12-21 13:49:59 -0500

    Test validation against sdmx-twg resources

--- sdmx/tests/writer/test_writer_xml.py
+++ sdmx/tests/writer/test_writer_xml.py
@@ -1,11 +1,15 @@
+import io
 import logging
+import zipfile
 
 import pytest
+import requests
 
 import sdmx
 from sdmx import message
 from sdmx.model import v21 as m
 from sdmx.model.v21 import DataSet, DataStructureDefinition, Dimension, Key, Observation
+from sdmx.writer.xml import etree
 from sdmx.writer.xml import writer as XMLWriter
 
 log = logging.getLogger(__name__)
@@ -261,3 +265,54 @@ def test_structure_roundtrip(pytestconfig, specimen, specimen_id, strict, tmp_pa
     assert msg0.compare(msg1, strict), (
         path.read_text() if pytestconfig.getoption("verbose") else path
     )
+
+
+@pytest.mark.network
+def test_install_schemas(tmp_path):
+    """Test that XSD files are downloaded and ready for use in validation."""
+    sdmx.install_schemas(schema_dir=tmp_path)
+
+    # Look for a couple of the expected files
+    files = ["SDMXCommon.xsd", "SDMXMessage.xsd"]
+    for schema_doc in files:
+        doc = tmp_path.joinpath(schema_doc)
+        assert doc.exists()
+
+
+@pytest.mark.network
+def test_validate_xml_from_samples(tmp_path):
+    """Use official samples to ensure validation work correctly."""
+    # Grab the latest v2.1 schema release to get the URL to the zip
+    release_url = "https://api.github.com/repos/sdmx-twg/sdmx-ml-v2_1/releases/latest"
+    gh_headers = {
+        "Accept": "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+    resp = requests.get(url=release_url, headers=gh_headers)
+    zipball_url = resp.json().get("zipball_url")
+    # Download the zipped content and find the schemas within
+    resp = requests.get(url=zipball_url, headers=gh_headers)
+    zipped = zipfile.ZipFile(io.BytesIO(resp.content))
+    zipped.extractall(path=tmp_path)
+    extracted_content = list(tmp_path.glob("sdmx-twg-sdmx-ml*"))[0]
+
+    # Schemas as just in a flat directory
+    schema_dir = extracted_content.joinpath("schemas")
+
+    # Samples are somewhat spread out, and some are known broken so we pick a bunch
+    samples_dir = extracted_content.joinpath("samples")
+    samples = [
+        samples_dir / "common" / "common.xml",
+        samples_dir / "demography" / "demography.xml",
+        samples_dir / "demography" / "esms.xml",
+        samples_dir / "exr" / "common" / "exr_common.xml",
+        samples_dir / "exr" / "ecb_exr_ng" / "ecb_exr_ng_full.xml",
+        samples_dir / "exr" / "ecb_exr_ng" / "ecb_exr_ng.xml",
+        samples_dir / "query" / "query_cl_all.xml",
+        samples_dir / "query" / "response_cl_all.xml",
+        samples_dir / "query" / "query_esms_children.xml",
+        samples_dir / "query" / "response_esms_children.xml",
+    ]
+
+    for sample in samples:
+        assert sdmx.validate_xml(sample, schema_dir)
