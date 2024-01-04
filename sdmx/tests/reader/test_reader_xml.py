9d85b3d6bd34b395ce40558a01b7f35be81bef1b
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2024-01-04 03:36:54 +0100

    Xfail read of esms_structured.xml
    This specimen cannot be read without its MSD.

--- sdmx/tests/reader/test_reader_xml.py
+++ sdmx/tests/reader/test_reader_xml.py
@@ -7,5 +7,8 @@ from sdmx.message import Message
 @pytest.mark.parametrize_specimens("path", format="xml")
 def test_read_xml(path):
     """XML specimens can be read."""
+    if "esms_structured" in path.name:
+        pytest.xfail("Not implemented")
+
     result = sdmx.read_sdmx(path)
     assert isinstance(result, Message)
