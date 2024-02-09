3256841d1f73c5f2996a785077d1b75884d47040
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2024-02-09 16:00:45 +0100

    Add classes for testing {v21,v30} URL construction

--- sdmx/tests/test_rest.py
+++ sdmx/tests/test_rest.py
@@ -23,3 +23,33 @@ class TestResource:
         assert re.fullmatch(
             "{actualconstraint .* vtlmappingscheme}", Resource.describe()
         )
+
+
+class TestURL:
+    """Common fixtures for testing URL classes."""
+
+    @pytest.fixture
+    def source(self):
+        from sdmx.source import Source
+
+        return Source(id="A0", url="https://example.com", name="Test source")
+
+
+class TestURLv21(TestURL):
+    """Construction of SDMX REST API 2.1 URLs."""
+
+    def test_join(self, source) -> None:
+        from sdmx.rest.v21 import URL
+
+        u = URL(source, resource_type=Resource.dataflow, resource_id="DF0")
+        assert "https://example.com/dataflow/A0/DF0/latest" == u.join()
+
+
+class TestURLv30(TestURL):
+    """Construction of SDMX REST API 3.0.0 URLs."""
+
+    def test_join(self, source) -> None:
+        from sdmx.rest.v21 import URL
+
+        u = URL(source, resource_type=Resource.dataflow, resource_id="DF0")
+        assert "https://example.com/dataflow/A0/DF0/latest" == u.join()
