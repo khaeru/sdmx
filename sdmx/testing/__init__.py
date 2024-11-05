124ccb379201e21fd952cd505bfbc82dc316903c
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2024-11-05 12:03:07 +0100

    Add specimen for #205

--- sdmx/testing/__init__.py
+++ sdmx/testing/__init__.py
@@ -291,6 +291,7 @@ class SpecimenCollection:
                 ("IMF_STA", "DSD_GFS.xml"),  # khaeru/sdmx#164
                 ("INSEE", "CNA-2010-CONSO-SI-A17-structure.xml"),
                 ("INSEE", "dataflow.xml"),
+                ("INSEE", "gh-205.xml"),
                 ("INSEE", "IPI-2010-A21-structure.xml"),
                 ("ISTAT", "22_289-structure.xml"),
                 ("ISTAT", "47_850-structure.xml"),
