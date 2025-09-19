25d2070eb1c0f4e0bc7cd3d3fe663225bb550e08
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2025-09-19 21:31:14 +0200

    Simplify .writer.csv
    - Don't create an instance of BaseWriter.
    - Collapse dataset() into the main method.
    - Drop code for creating DATAFLOW column, now handled by
      .convert.pandas.

--- sdmx/writer/csv.py
+++ sdmx/writer/csv.py
@@ -1,24 +1,24 @@
-"""SDMX-CSV 1.0 writer.
+"""SDMX-CSV writer.
 
 See :ref:`sdmx-csv`.
 """
 
 from os import PathLike
-from typing import Literal, Optional, Union
+from typing import TYPE_CHECKING, Optional, Union
 
 import pandas as pd
 
-from sdmx import urn
-from sdmx.model import v21 as model
+from sdmx.convert.pandas import PandasConverter
+from sdmx.format.csv import common, v1
 
-from .base import BaseWriter
-
-writer = BaseWriter("csv")
+if TYPE_CHECKING:
+    from sdmx.message import DataMessage
+    from sdmx.model.common import BaseDataSet
 
 
 def to_csv(
-    obj,
-    *args,
+    obj: Union["DataMessage", "BaseDataSet"],
+    *,
     path: Optional[PathLike] = None,
     rtype: type[Union[str, pd.DataFrame]] = str,
     **kwargs,
@@ -36,7 +36,6 @@ def to_csv(
     ----------
     path : os.PathLike, optional
         Path to write an SDMX-CSV file.
-        If given, nothing is returned.
     rtype :
         Return type; see below. Pass literally ``str`` or ``pd.DataFrame``; *not* an
         instance of either class.
@@ -44,10 +43,13 @@ def to_csv(
     Other parameters
     ----------------
     kwargs :
-        Keyword arguments passed to :func:`.dataset`.
+        Keyword arguments passed to :func:`.PandasConverter` and
+        :class:`.format.csv.v1.FormatOptions`.
 
     Returns
     -------
+    None :
+        if `path` is given.
     str :
         if `rtype` is :class:`str`.
     pd.DataFrame :
@@ -59,11 +61,32 @@ def to_csv(
         If `obj` is any class except :class:`.DataSet`; this is the only class for which
         the SDMX-CSV standard describes a format.
 
+        The two optional parameters are exactly as described in the specification.
+
+        Because SDMX-CSV includes a ``DATAFLOW`` column with an identifier (partial URN) for
+        the dataflow to which the data conform, it is mandatory that the
+        :attr:`~.DataSet.described_by` attribute of `obj` gives an association to a
+        :class:`.DataflowDefinition` object, from which a :mod:`.urn` can be constructed.
+
+    NotImplementedError
+        For ``labels="both"`` or ``time_format="normalized"``.
+    ValueError
+        If :attr:`.DataSet.described_by` is :data:`None`.
+
     See also
     --------
     :ref:`sdmx.writer.csv <writer-csv>`.
     """
-    result = writer.recurse(obj, *args, **kwargs)
+    # Extract format options from `kwargs`, store as an instance of FormatOptions
+    fo = dict(
+        labels=kwargs.pop("labels", None) or common.CSVFormatOptions.labels,
+        time_format=kwargs.pop("time_format", common.CSVFormatOptions.time_format),
+    )
+    kwargs["format"] = kwargs.get("format", None) or v1.FORMAT
+    kwargs.setdefault("format_options", v1.FormatOptions(**fo))
+
+    # Use PandasConverter for the conversion
+    result = PandasConverter(**kwargs).convert(obj)
 
     if path:
         return result.to_csv(path, index=False)
@@ -73,82 +96,3 @@ def to_csv(
         return result
     else:
         raise ValueError(f"Invalid rtype={rtype!r}")
-
-
-@writer
-def dataset(
-    obj: model.DataSet,
-    *,
-    labels: Literal["id", "both"] = "id",
-    time_format: Literal["original", "normalized"] = "original",
-    **kwargs,
-) -> pd.DataFrame:
-    """Convert :class:`.DataSet`.
-
-    The two optional parameters are exactly as described in the specification.
-
-    Because SDMX-CSV includes a ``DATAFLOW`` column with an identifier (partial URN) for
-    the dataflow to which the data conform, it is mandatory that the
-    :attr:`~.DataSet.described_by` attribute of `obj` gives an association to a
-    :class:`.DataflowDefinition` object, from which a :mod:`.urn` can be constructed.
-
-    Parameters
-    ----------
-    labels : "id" or "both", optional
-        “id”
-           Display only :attr:`Dimension.id` / :attr:`DataAttribute.id` in column
-           headers and :attr:`Code.id` in data rows.
-        “both”
-           Display both the ID and the localized :attr:`NameableArtefact.name`. Not yet
-           implemented.
-    time_format : "original" or "normalized", optional
-        “original”
-           Values for any dimension or attribute with ID ``TIME_PERIOD`` are displayed
-           as recorded.
-        “normalized”
-           ``TIME_PERIOD`` values are converted to the most granular ISO 8601
-           representation taking into account the highest frequency of the data in the
-           message and the moment in time when the lower-frequency values were
-           collected. Not yet implemented.
-
-        This parameter is called `timeFormat` in the spec and in HTTP Accept headers.
-
-    Other parameters
-    ----------------
-    kwargs :
-        Keyword arguments passed to :func:`.to_pandas`. In particular, `attributes` is
-        useful to control which attribute values are included in the returned CSV.
-
-    Returns
-    -------
-    pandas.DataFrame
-
-    Raises
-    ------
-    NotImplementedError
-        For ``labels="both"`` or ``time_format="normalized"``.
-    ValueError
-        If :attr:`.DataSet.described_by` is :data:`None`.
-    """
-    from sdmx.convert.pandas import PandasConverter
-
-    if labels == "both":
-        raise NotImplementedError(f"labels={labels}")
-    elif time_format != "original":
-        raise NotImplementedError(f"time_format={time_format}")
-
-    # Use PandasConverter for the conversion
-    tmp = (
-        PandasConverter(**kwargs)
-        .convert(obj)
-        .reset_index()
-        .rename(columns={"value": "OBS_VALUE"})
-    )
-
-    # Construct the DATAFLOW column
-    if obj.described_by is None:
-        raise ValueError(f"No associated data flow definition for {obj}")
-    dfd_urn = urn.make(obj.described_by).split("=", maxsplit=1)[1]
-    df_col = pd.Series(dfd_urn, index=tmp.index, name="DATAFLOW")
-
-    return pd.concat([df_col, tmp], axis=1)
