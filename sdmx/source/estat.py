c3d2651c3ed37f1030ee5b1a5a4f4dd84bebae7e
Ben Frankel <ben.frankel7@gmail.com>
2025-11-11 16:39:38 -0800

    Attempt to fix CI

--- sdmx/source/estat.py
+++ sdmx/source/estat.py
@@ -36,7 +36,8 @@ def handle_references_param(kwargs: dict) -> None:
         kwargs.get("resource", None), "id", None
     )
     params = kwargs.setdefault("params", {})
-    params["references"] = kwargs.pop("references", None)
+    if "references" in kwargs:
+        params["references"] = kwargs["references"]
 
     # Preempt default values that would be set by Client._request_from_args()
     if not params.get("references"):
