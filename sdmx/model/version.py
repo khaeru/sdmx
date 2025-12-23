68b8e3ba533cb07db851f75bc8b236ab78ebd259
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2025-12-22 23:35:58 -0500

    Improve Version class
    - Inherit from packaging.version._BaseVersion, rather than .Version
    - Use a dataclass.
    - Simplify and improve .increment()
      - Clear 'inferior' components or parts if a 'superior' part is
        incremented.
    - Improve type hints.

--- sdmx/model/version.py
+++ sdmx/model/version.py
@@ -1,6 +1,9 @@
 import operator
 import re
 from collections.abc import Callable
+from dataclasses import InitVar, dataclass, field, replace
+from functools import cache
+from itertools import zip_longest
 
 import packaging.version
 
@@ -8,18 +11,15 @@ import packaging.version
 #:
 #: - :py:`"2_1"` SDMX 2.1, e.g. "1.0"
 #: - :py:`"3_0"` SDMX 3.0, e.g. "1.0.0-draft"
-#: - :py:`"py"` Python-compatible versions, using :mod:`packaging.version`.
 VERSION_PATTERNS = {
     "2_1": re.compile(r"^(?P<release>[0-9]+(?:\.[0-9]+){1})$"),
     "3_0": re.compile(r"^(?P<release>[0-9]+(?:\.[0-9]+){2})(-(?P<ext>.+))?$"),
-    "py": re.compile(
-        r"^\s*" + packaging.version.VERSION_PATTERN + r"\s*$",
-        re.VERBOSE | re.IGNORECASE,
-    ),
 }
 
 
 def _cmp_method(op: Callable) -> Callable:
+    """Shorthand for comparison methods."""
+
     def cmp(self, other) -> bool:
         try:
             return op(self._key, other._key)
@@ -32,90 +32,106 @@ def _cmp_method(op: Callable) -> Callable:
     return cmp
 
 
-class Version(packaging.version.Version):
+def _release_component(index: int) -> Callable[["Version"], int]:
+    """Shorthand for properties that access release components."""
+
+    def getter(self) -> int:
+        return self.release[index] if len(self.release) >= index + 1 else 0
+
+    return getter
+
+
+@dataclass(unsafe_hash=True, slots=True)
+class Version(packaging.version._BaseVersion):
     """Class representing a version.
 
     The SDMX Information Model **does not** specify a Version class; instead,
     :attr:`.VersionableArtefact.version` is described as “a version **string** following
     SDMX versioning rules.”
 
-    In order to simplify application of those ‘rules’, and to handle the differences
-    between SDMX 2.1 and 3.0.0, this class extends :class:`packaging.version.Version`,
-    which provides a complete interface for interacting with Python version specifiers.
-    The extensions implement the particular form of versioning laid out by the SDMX
-    standards. Specifically:
+    In order to simplify application of those ‘rules’, and to handle differences between
+    SDMX 2.1 and 3.x, this class extends :class:`packaging.version._BaseVersion`, and
+    thus is comparable with :class:`packaging.version.Version`. It implements the
+    particular form of version specifiers laid out by the SDMX standards. Specifically:
 
-    - :attr:`kind` as added to identify whether a Version instance is an SDMX 2.1, SDMX
-      3.0, or Python-style version string.
-    - Attribute aliases for particular terms used in the SDMX 3.0 standards:
-      :attr:`patch` and :attr:`ext`.
+    - :attr:`kind` identifies whether a Version instance is an SDMX 2.1, SDMX 3.x, or
+      Python-style version string.
+    - :attr:`patch` and :attr:`ext` attributes match the particular terms used in the
+      SDMX 3.0 standards.
     - The :class:`str` representation of a Version uses the SDMX 3.0 style of separating
-      the :attr:`ext` with a hyphen ("1.0.0-dev1"), which differs from the Python style
-      of using no separator for a ‘post-release’ ("1.0.0dev1") or a plus symbol for a
-      ‘local part’ ("1.0.0+dev1").
+      the :attr:`ext` with a hyphen ("1.0.0-dev1"). This differs from the Python
+      specification, which uses either (a) no separator for a ‘pre-release’ ("1.0rc1"),
+      (b) a period for a ‘post-’ and/or ‘development release’ ("1.0.post2.dev3"), or
+      (c) plus and period symbols for ‘local parts’ ("1.0+local1.local2").
     - :meth:`increment`, an added convenience method.
     - The class is comparable and interchangeable with :class:`str` version expressions.
 
     Parameters
     ----------
-    version : str
-        String expression
+    value :
+        String version expression.
     """
 
-    #: Type of version expression; one of the keys of :data:`.VERSION_PATTERNS`.
-    kind: str
+    value: InitVar[str | None] = None
 
-    def __init__(self, version: str):
+    #: :py:`"2_1"` or :py:`"3_0"` for SDMX-compatible versions. :py:`"py"` for a Python
+    #: version specifier.
+    kind: str = "py"
+
+    #: Same as :attr:`packaging.version.Version.epoch`.
+    epoch: int = 0
+    #: Same as :attr:`packaging.version.Version.release`.
+    release: tuple[int, ...] = field(default=(0,))
+    #: Same as :attr:`packaging.version.Version.pre`.
+    pre: tuple[str, int] | None = None
+    #: Same as :attr:`packaging.version.Version.post`.
+    post: tuple[str, int] | None = None
+    #: Same as :attr:`packaging.version.Version.dev`.
+    dev: tuple[str, int] | None = None
+    #: Same as :attr:`packaging.version.Version.local`.
+    local: tuple[str | int, ...] = field(default_factory=tuple)
+
+    def __post_init__(self, value: str | None) -> None:
+        # Parse as a SDMX-compatible version
         for kind, pattern in VERSION_PATTERNS.items():
-            match = pattern.fullmatch(version)
-            if match:
+            if value is None:
+                break
+            if match := pattern.fullmatch(value):
+                self.kind = kind
+
+                # Store the parsed out pieces of the version
+                gd = match.groupdict()
+                self.release = tuple(int(i) for i in gd.pop("release").split("."))
+                ext = gd.pop("ext", None)
+                self.local = (ext,) if ext else tuple()
+
+                # No further parsing
+                value = None
                 break
 
-        if not match:
-            raise packaging.version.InvalidVersion(version)
+        if value is not None:
+            # Parse `value` as if it were a ordinary Python version specifier
+            self.kind = "py"
 
-        self.kind = kind
+            v = packaging.version.Version(value)  # Raises on an invalid expression
 
-        if kind == "py":
-            tmp = packaging.version.Version(version)
-            self._version = tmp._version
-        else:
-            # Store the parsed out pieces of the version
-            try:
-                ext = match.group("ext")
-                local = None if ext is None else (ext,)
-            except IndexError:
-                local = None
-            self._version = packaging.version._Version(
-                epoch=0,
-                release=tuple(int(i) for i in match.group("release").split(".")),
-                pre=None,
-                post=None,
-                dev=None,
-                local=local,
-            )
+            # Update fields with the parsed segments and components
+            self.epoch = v.epoch
+            self.release = v.release
+            self.pre = v._version.pre
+            self.post = v._version.post
+            self.dev = v._version.dev
+            self.local = v._version.local or ()
 
-        self._update_key()
-
-    def _update_key(self):
-        # Generate a key which will be used for sorting
+        # Set _BaseVersion._key for comparison
         self._key = packaging.version._cmpkey(
-            self._version.epoch,
-            self._version.release,
-            self._version.pre,
-            self._version.post,
-            self._version.dev,
-            self._version.local,
+            self.epoch,
+            self.release,
+            self.pre,
+            self.post,
+            self.dev,
+            self.local if self.local else None,
         )
-
-    def __str__(self):
-        if self.kind == "3_0":
-            parts = [".".join(str(x) for x in self.release)]
-            if self.ext:
-                parts.append(f"-{self.ext}")
-            return "".join(parts)
-        else:
-            return super().__str__()
 
     __eq__ = _cmp_method(operator.eq)
     __ge__ = _cmp_method(operator.ge)
@@ -124,99 +140,124 @@ class Version(packaging.version.Version):
     __lt__ = _cmp_method(operator.lt)
     __ne__ = _cmp_method(operator.ne)
 
-    @property
-    def patch(self) -> int:
-        """Alias for :any:`Version.micro <packaging.version.Version.micro>`."""
-        return self.micro
+    major = property(_release_component(0))
+    minor = property(_release_component(1))
+    patch = property(_release_component(2))
+    micro = property(_release_component(2))
 
     @property
     def ext(self) -> str | None:
         """SDMX 3.0 version 'extension'.
 
-        For :py:`kind="py"`, this is equivalent to :attr:`Version.local
-        <packaging.version.Version.local>`.
+        For :py:`kind="py"`, this is equivalent to
+        :attr:`packaging.version.Version.local`.
         """
-        if self._version.local is None:
-            return None
-        else:
-            return "".join(map(str, self._version.local))
+        return "".join(map(str, self.local)) if self.local else None
 
-    def increment(self, **kwargs: bool | int) -> "Version":
+    def increment(
+        self,
+        major: bool | int | None = None,
+        minor: bool | int | None = None,
+        patch: bool | int | None = None,
+        ext: bool | int | None = None,
+        *,
+        micro: bool | int | None = None,
+        local: bool | int | None = None,
+    ) -> "Version":
         """Return a Version that is incrementally greater than the current Version.
 
-        If no arguments are given, then by default :py:`minor=True` and :py:`ext=1`.
+        Each argument may be one of:
+
+        - :class:`int`: the respective release component or extension is incremented by
+          this amount. If the argument is :py:`0`, the current value is preserved
+          exactly.
+        - :any:`True`: same as :py:`1`.
+        - :any:`False`: same as :py:`0`.
+        - :any:`None` (default): if any 'larger' component is incremented, zero the
+          respective component. For instance, if :py:`major=1, minor=None`, then the
+          major release component is incremented, and the minor release component is
+          reset to :py:`0`.
+
+        If no arguments are given, then by default :py:`minor=1` and :py:`ext=1`.
 
         Parameters
         ----------
-        major : bool or int, optional
+        major :
             If given, increment the :attr:`Version.major
-            <packaging.version.Version.major>` part.
-        minor : bool or int, optional
+            <packaging.version.Version.major>` component of the :attr:`release` segment
+            by this amount.
+        minor :
             If given, increment the :attr:`Version.minor
             <packaging.version.Version.minor>` part.
-        patch : bool or int, optional
-            If given, increment the :attr:`.Version.patch` part.
-        micro : bool or int, optional
-            Alias for `patch`.
-        ext : bool or int, optional
+        patch :
+            If given, increment the :attr:`.Version.patch` part. The keyword argument
+            `patch` may be used as an alias.
+        ext :
             If given, increment the :attr:`.Version.ext` part. If this part is not
-            present, add "dev1".
-        local: bool or int, optional
-            Alias for `ext`.
+            present, add "dev{ext}". The keyword argument `local` may be used as an
+            alias.
         """
-        if not kwargs:
-            # Apply defaults
-            kwargs["minor"] = kwargs["ext"] = 1
+        # Handle aliases
+        if micro is not None:
+            assert patch is None
+            patch = micro
+        if local is not None:
+            assert ext is None
+            ext = local
 
-        # Convert self._version.release into a mutable dict
-        N_release = len(self._version.release)  # Number of parts in `release` tuple
-        parts = dict(
-            major=self._version.release[0] if N_release > 0 else 0,
-            minor=self._version.release[1] if N_release > 1 else 0,
-            patch=self._version.release[2] if N_release > 2 else 0,
-        )
-        # Convert self._version.local into a mutable list
-        local = list(self._version.local) if self._version.local is not None else []
+        # Apply defaults
+        if major is minor is patch is ext is None:
+            minor = ext = 1
 
-        # Increment parts according to kwargs
-        for part, value in kwargs.items():
-            # Recognize kwarg aliases
-            part = {"local": "ext", "micro": "patch"}.get(part, part)
+        # Increment release parts
+        release, incremented = [], False
+        for current, arg in zip_longest(
+            self.release, (major, minor, patch), fillvalue=0
+        ):
+            match arg:
+                case None:
+                    # If a superior part has been incremented, None → reset to 0
+                    release.append(0 if incremented else current)
+                case int() | bool():
+                    release.append(current + int(arg))
+                    incremented = True
 
-            # Update the extension/local part
-            if part == "ext":
-                if not len(local):
-                    ext = "dev1"
-                elif match := re.fullmatch("([^0-9]+)([0-9]+)", str(local[0])):
-                    _l, _n = match.group(1, 2)
-                    ext = f"{_l}{int(_n) + value}"
-                else:
-                    raise NotImplementedError(
-                        f"Increment SDMX version extension {self.ext!r}"
-                    )
-                local = [ext]
-                continue
+        if ext is None:
+            # Clear the local part
+            _local = []
+        else:
+            # Increment extension/local part
+            # Convert self.local into a mutable list, or supply a base value
+            _local = list(self.local) or ["dev0"]
 
-            try:
-                # Update the major/minor/patch parts
-                parts[part] += int(value)
-            except KeyError:
-                raise ValueError(f"increment(..., {part}={value})")
+            if match := re.fullmatch("([^0-9]+)([0-9]+)", str(_local[0])):
+                # Increment given and the first local part has an integer suffix
+                _l, _n = match.group(1, 2)
+                _local[0] = f"{_l}{int(_n) + int(ext)}"
+            else:
+                raise NotImplementedError(f"Increment SDMX version extension {_local}")
 
-        # Construct a new Version object
-        result = type(self)(str(self))
-        # Overwrite its private _version attribute and key
-        result._version = packaging.version._Version(
-            epoch=self._version.epoch,
-            release=tuple(parts.values()),
-            pre=self._version.pre,
-            post=self._version.post,
-            dev=self._version.dev,
-            local=tuple(local) if len(local) else None,
-        )
-        result._update_key()
+        # Return a new instance with modified release and local attributes
+        return replace(self, release=tuple(release), local=tuple(_local))
 
-        return result
+    @cache
+    def __str__(self) -> str:
+        # Same as packaging.version.Version.__str__, except using the SDMX "-" separator
+        # for the local part
+        local_sep = "+" if self.kind == "py" else "-"
+        parts = [
+            f"{self.epoch}!" if self.epoch else None,
+            ".".join(map(str, self.release)),
+            "{}{}".format(*self.pre) if self.pre else None,
+            ".{}{}".format(*self.post) if self.post else None,
+            ".{}{}".format(*self.dev) if self.dev else None,
+            local_sep + ".".join(map(str, self.local)) if self.local else None,
+        ]
+        return "".join(filter(None, parts))
+
+    @cache
+    def __repr__(self) -> str:
+        return f"<Version('{self!s}')>"
 
 
 def increment(value: packaging.version.Version | str, **kwargs) -> Version:
